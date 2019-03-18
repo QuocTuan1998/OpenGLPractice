8d747160f0febab87608d568bf3b42909027054a
Nguyen Quoc Tuan <quoctuanb6@gmail.com>
2019-03-18 15:44:49 +0700

    Vertex buffer and drawing triangle

--- FirstOpenGL/src/Application.cpp
+++ FirstOpenGL/src/Application.cpp
@@ -11,11 +11,6 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
-	if (glewInit() != GLEW_OK)
-	{
-		
-	}
-
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -27,17 +22,32 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f, 0.5f,
+		 0.5f, -0.5f
+	};
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); 
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		/*glBegin(GL_TRIANGLES);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(0.0f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glEnd();*/
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
